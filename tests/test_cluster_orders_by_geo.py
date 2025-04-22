ef0947a7748d527118474408bed21a7450f4ae93
sriramjothimani <sriramjothimani@gmail.com>
2025-04-22 23:53:25 +0530

    Refer test data from a common folder

--- tests/test_cluster_orders_by_geo.py
+++ tests/test_cluster_orders_by_geo.py
@@ -4,15 +4,14 @@ from delivery_management.tools.cluster_orders import ClusterOrdersByGeoTool  # R
 
 # Test cases for ClusterOrdersByGeoTool
 
-BASE_DIR = Path(__file__).resolve().parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 def test_cluster_order():
     clusterOrdersTool = ClusterOrdersByGeoTool()\
-            .with_orders_file(Path(BASE_DIR / "data/orders.json"))\
-            .with_geolocations_file(Path(BASE_DIR / "data/geolocations.json"))\
-            .with_static_ref_file(Path(BASE_DIR / "data/static_reference_data.json"))\
-            .with_inventory_file(Path(BASE_DIR / "data/inventory.json"))
+            .with_orders_file(Path(BASE_DIR / "src/delivery_management/data/orders.json"))\
+            .with_geolocations_file(Path(BASE_DIR / "src/delivery_management/data/geolocations.json"))\
+            .with_static_ref_file(Path(BASE_DIR / "src/delivery_management/data/static_reference_data.json"))\
+            .with_inventory_file(Path(BASE_DIR / "src/delivery_management/data/inventory.json"))
     result = clusterOrdersTool.run()
     json = result.model_dump()
     print(json)
